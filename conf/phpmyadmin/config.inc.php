41b3d8011f51c9b149bb466f590379be9f55a83f
Rodrigo <rodrigo.martinez@escrol.es>
2022-07-28 10:17:56 +0200

    Inicio

---
+++ conf/phpmyadmin/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$i = 0;
+
+/**
+ * First server
+ */
+$i++;
+/* Authentication type */
+/*$cfg['Servers'][$i]['ssl'] = true;
+$cfg['Servers'][$i]['ssl_ca'] = '/etc/certs/root-ca.pem';
+$cfg['Servers'][$i]['ssl_cert'] = '/etc/certs/server.pem';
+$cfg['Servers'][$i]['ssl_key'] = '/etc/certs/server.pem';*/
